8567f03117d1cc827bf65ba1300baa678c00135f
EduardoAlcaraz <eddiealcaraz2001@yahoo.com>
2022-04-13 01:18:30 -0700

    Difficulty

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,11 +1,13 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
+        super(scene, x, y, texture, frame);
   
-      // add object to existing scene
-      scene.add.existing(this);
-      this.isFiring = false;
-      this.moveSpeed = 2;
+        // add object to existing scene
+        scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
+
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update(){
@@ -19,8 +21,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
             this.isFiring = true;
+            this.sfxRocket.play();
         }
         //move up when fired
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
@@ -31,5 +34,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
             this.isFiring = false;
             this.y = game.config.height - borderUISize - borderPadding;
         }
+    }
+
+    // reset rocket to "ground"
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
